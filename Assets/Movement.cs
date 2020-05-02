2ad7a23e25757cb0012ffcd12d200af87cb9cd86
JiayiXuu <jiayixu@ad.unc.edu>
2020-05-02 16:05:53 -0400

    Added place scene, 2D minimap, icons

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float translationalSpeed = 1.0f;
+    private float rotationalSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        this.transform.position += (Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0) * translationalSpeed * new Vector3(this.transform.forward.x, 0, this.transform.forward.z) +
+        (Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0) * translationalSpeed * new Vector3(this.transform.right.x, 0, this.transform.right.z);
+
+        this.transform.eulerAngles = new Vector3(0, this.transform.eulerAngles.y + Input.GetAxis("Mouse X") * rotationalSpeed, 0);
+    }
+}
---
+++ Assets/TreasureHunter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; //<---------------------THIS MIGHT BREAK WHEN BUILDING :) BECAUSE THE EDITOR IS NOT AVALIABLE WHEN BUILDING :) FIND ANOTHER WAY MUH MAN
+
+public class TreasureHunter : MonoBehaviour
+{
+    //  public TextMesh updateText;
+    // public TextMesh itemNumerText; 
+    // public TextMesh scoreText; 
+    private int numItems = 0; 
+
+    private int score = 0; 
+
+   
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {   
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if(other.CompareTag("Collectible")){
+                
+            
+
+                numItems ++; 
+
+                // itemNumerText.text = "Items Collected: " + numItems;
+                // score = score + other.GetComponent<Collectible>().pointVal; 
+                // scoreText.text = "Score: " + score; 
+                Destroy(other.GetComponent<Collider>().gameObject);
+
+        }
+           
+    }
+
+
+
+}
