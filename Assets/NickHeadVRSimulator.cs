ab23f8117b922d98239c30816b1ab2d4a92bb16f
Nicholas Day <nick@tutorialsbynick.com>
2020-05-03 12:35:41 -0400

    3d minimap potential progress

---
+++ Assets/NickHeadVRSimulator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NickHeadVRSimulator : MonoBehaviour
+{
+    public float translationalSpeed = 0.1f;
+    public float rotationalSpeed = 2.0f;
+    public bool shouldLockMouse = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = shouldLockMouse ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += (Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0) * translationalSpeed * new Vector3(this.transform.forward.x, 0, this.transform.forward.z) +
+        (Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0) * translationalSpeed * new Vector3(this.transform.right.x, 0, this.transform.right.z);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+
+        //only yaw should change for aerial view
+        //only using mouse x b/c left/right makes more sense for aerial yaw than up/down
+        this.transform.eulerAngles = new Vector3(0, this.transform.eulerAngles.y + Input.GetAxis("Mouse X") * rotationalSpeed, 0);
+    }
+}
---
+++ Assets/ThreeDMinimap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDMinimap : MonoBehaviour
+{
+    public Collectible[] Collectibles;
+    public GameObject[] miniCollectibles;
+    public float scale = .01f;
+    public float forward = 2f;
+    public float right = 2f;
+    void Start()
+    {
+        for (int i = 0; i < Collectibles.Length; i++)
+        {
+            Collectible c = Collectibles[i];
+            GameObject g = c.gameObject;
+            //g.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            miniCollectibles[i] = Instantiate(g, new Vector3(1f, 1f, 1f), g.transform.rotation);
+            miniCollectibles[i].transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Collectibles.Length; i++)
+        {
+            Collectible c = Collectibles[i];
+            GameObject g2 = c.gameObject;
+            GameObject g = miniCollectibles[i];
+            g.transform.position = Camera.main.transform.position + Camera.main.transform.forward * forward + Camera.main.transform.right * right + g2.transform.position * scale;
+        }
+    }
+}
