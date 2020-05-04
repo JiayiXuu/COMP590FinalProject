04b49dd45d94c343f2d24f5fd82299927990cf75
Nicholas Day <nick@tutorialsbynick.com>
2020-05-03 23:23:16 -0400

    Add capsule representing player position

--- Assets/ThreeDMinimap.cs
+++ Assets/ThreeDMinimap.cs
@@ -6,9 +6,13 @@ public class ThreeDMinimap : MonoBehaviour
 {
     public Collectible[] Collectibles;
     public GameObject[] miniCollectibles;
+
+    GameObject playerRep;
+
     public float scale = .01f;
     public float forward = 2f;
     public float right = 2f;
+    public float up = 2f;
     void Start()
     {
         for (int i = 0; i < Collectibles.Length; i++)
@@ -19,6 +23,8 @@ public class ThreeDMinimap : MonoBehaviour
             miniCollectibles[i] = Instantiate(g, new Vector3(1f, 1f, 1f), g.transform.rotation);
             miniCollectibles[i].transform.localScale = new Vector3(scale, scale, scale);
         }
+        playerRep = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        playerRep.transform.localScale = new Vector3(scale, scale, scale);
     }
 
     // Update is called once per frame
@@ -29,7 +35,8 @@ public class ThreeDMinimap : MonoBehaviour
             Collectible c = Collectibles[i];
             GameObject g2 = c.gameObject;
             GameObject g = miniCollectibles[i];
-            g.transform.position = Camera.main.transform.position + Camera.main.transform.forward * forward + Camera.main.transform.right * right + g2.transform.position * scale;
+            g.transform.position = Camera.main.transform.position + Camera.main.transform.up * up + Camera.main.transform.forward * forward + Camera.main.transform.right * right + g2.transform.position * scale;
         }
+        playerRep.transform.position = Camera.main.transform.position + Camera.main.transform.up * up + Camera.main.transform.forward * forward + Camera.main.transform.right * right + Camera.main.transform.position * scale;
     }
 }
